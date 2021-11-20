5de73e8116a0ac8f1ea4eee2726df65144bf0f51
Fabio Lettieri <fabiolettieri09@gmail.com>
2021-11-20 15:53:24 -0300

    Implementação e usabilidade do CheckBox

--- app/src/main/java/com/flettieri/componentesbasicos/MainActivity.java
+++ app/src/main/java/com/flettieri/componentesbasicos/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -16,6 +17,11 @@ public class MainActivity extends AppCompatActivity {
     private TextInputEditText campoEmail;
     private TextView textoResultado;
 
+    private CheckBox checkVerde;
+    private CheckBox checkBranco;
+    private CheckBox checkVermelho;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,13 +31,40 @@ public class MainActivity extends AppCompatActivity {
         campoEmail = findViewById(R.id.editEmail);
         textoResultado = findViewById(R.id.textView);
 
+        checkVerde = findViewById(R.id.checkVerde);
+        checkBranco = findViewById(R.id.checkBranco);
+        checkVermelho = findViewById(R.id.checkVermelho);
+
+    }
+
+    public void checkbox() {
+        String texto = "";
+        if (checkVerde.isChecked()) {
+            texto = checkVerde.getText().toString();
+        }
+
+        if (checkBranco.isChecked()) {
+            texto = checkBranco.getText().toString();
+        }
+
+        if (checkVermelho.isChecked()) {
+            texto = checkVermelho.getText().toString();
+        }
+
+        textoResultado.setText(texto);
+
+
     }
 
     public void enviar(View view) {
+        checkbox();
+
+        /*
         String nome = campoNome.getText().toString();
         String email = campoEmail.getText().toString();
 
         textoResultado.setText("Nome: " + nome + "\nE-mail: " + email);
+         */
     }
 
     public void limpar(View view) {
